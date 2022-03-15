542cd797f4252e0084a4dc22136533b3c7c50b41
Alex Athanasopoulos <alex@melato.org>
2022-03-15 23:01:43 +0200

    added FullOptions

--- options.go
+++ options.go
@@ -11,6 +11,11 @@ type Options struct {
 	Format string `name:"format" usage:"table format: csv|fixed"`
 }
 
+type FullOptions struct {
+	Options
+	Columns string `name:"columns" usage:"comma-separated list of columns to use"`
+}
+
 func (opt *Options) NewWriterf(w io.Writer) (Writer, error) {
 	var writer Writer
 	format := opt.Format
@@ -40,6 +45,23 @@ func (opt *Options) NewWriterWithColumns(columnSpec string, allColumns ...*Colum
 	columns := allColumns
 	if columnSpec != "" {
 		columns, err = SelectColumns(allColumns, strings.Split(columnSpec, ",")...)
+		if err != nil {
+			PrintColumns(allColumns)
+			return nil, err
+		}
+	}
+	w.Columns(columns...)
+	return w, nil
+}
+
+func (opt *FullOptions) NewWriter(allColumns ...*Column) (Writer, error) {
+	w, err := opt.NewWriterf(os.Stdout)
+	if err != nil {
+		return nil, err
+	}
+	columns := allColumns
+	if opt.Columns != "" {
+		columns, err = SelectColumns(allColumns, strings.Split(opt.Columns, ",")...)
 		if err != nil {
 			PrintColumns(allColumns)
 			return nil, err
